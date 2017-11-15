2c988ed2d69db7be0899685130c12526158c659d
rohitner <rohitner1@gmail.com>
2017-11-15 13:54:32 +0530

    spline interpolation of data

--- plotedmonton.r
+++ plotedmonton.r
@@ -1,4 +1,7 @@
 data = read.csv(file="edmonton.csv", header=FALSE, sep=",")
 colnames(data)=NULL
-plot(data[,6])
-plot(data[,3])
+plot(data[,1])
+for (count in c(1:dim(data)[1]))
+{
+    lines(predict(smooth.spline(data[,count],spar=0.01),seq(1,length(data[,count]),len=1000)))
+}
