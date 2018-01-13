86c8771da16e07a140640e5d644af7446dd235bb
rohitner <rohitner1@gmail.com>
2018-01-14 01:51:16 +0530

    ubuntu reset commit

--- fpca.py
+++ fpca.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
-from scipy.interpolate import UnivariateSpline
+from scipy.interpolate import interp1d, BSpline
+from sklearn.decomposition import PCA
 import csv
 import argparse
 
@@ -13,28 +13,20 @@ INPUT_FILENAME = args.input
 
 X = []
 with open(INPUT_FILENAME) as csvfile:
-    reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC) # change contents to floats
-    for row in reader: # each row is a list
+    reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
+    # change contents to floats
+    for row in reader:
         X.append(row)
 
-X_std = StandardScaler().fit_transform(X)
-for k in range(1,16):
-    x=np.arange(0,31)
-    spl = UnivariateSpline(x, X_std[k-1])
-    spl.set_smoothing_factor(0.1)
-    print(spl.get_coeffs(),"\n",spl.get_knots(),"\n");
-    xs=np.linspace(0,30,1000)
-    plt.plot(xs, spl(xs), 'b', lw=0.5)               # we can use the spl as a function
+feature = np.array([])
+for i in range (0, 15):
+    c = interp1d(np.arange(0, 31), X[i], kind = "cubic")
+    # feature vector corresponding to ith record
+    feature = np.append(feature, c._spline.tck[1])
 
-u,s,v = np.linalg.svd(X_std.T)
-eig_vecs=u
-eig_vals=[]
-cov_mat = np.cov(X_std.T)
-for i in range (0,31):
-    eig_vals.append(np.divide(cov_mat.dot(u.T[i]),u.T[i])[0])
-
-eig_pairs = [(np.abs(eig_vals[i]), u.T[i]) for i in range(len(eig_vals))]
-spl_f=UnivariateSpline(x,eig_pairs[0][1])
-spl_f.set_smoothing_factor(0.1)
-plt.plot(xs,spl_f(xs),'r',lw=0.5)
-plt.show()
+feature = np.reshape(feature, (15, 31))
+# pca = PCA()
+# pca.fit(feature)
+# print(np.shape(pca.components_))
+W, v = np.linalg.eig(np.cov(feature.T))
+W, v = W.real, v.real
